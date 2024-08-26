a8110e0a03a1756ba732c1ef93a64ee60fa74ba5
gavinbarragancd /Users/gavinbarragan/cs121-2024-sp-activity1-gavinbarragan/cs121-2024-sp-activity2-gavinbarragan/a2.1-TemperatureConversion/ && javac TemperatureConversion.java && java TemperatureConversion <gavinbarragan@u.boisestate.edu>
2024-08-26 12:40:10 -0600

    worked on constructor

---
+++ MagicSquare.java
@@ -0,0 +1,18 @@
+public class MagicSquare {
+
+    private int n;
+    private int[][] magicGrid;
+    private int magicNumber;
+
+    public MagicSquare(int n) {
+        magicNumber = n * (n*n + 1) / 2;
+        magicGrid = new int[n][n];
+        for (int r = 0;r < n;r++) {
+            for (int c=0;c<n; c++) {
+                while (magicGrid==null) {
+                    
+                }
+            }
+        }
+    }
+}
