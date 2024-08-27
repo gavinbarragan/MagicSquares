2bcaf4972b6e98eab9b02f4a1f21144c3a6b474b
gavinbarragancd /Users/gavinbarragan/cs121-2024-sp-activity1-gavinbarragan/cs121-2024-sp-activity2-gavinbarragan/a2.1-TemperatureConversion/ && javac TemperatureConversion.java && java TemperatureConversion <gavinbarragan@u.boisestate.edu>
2024-08-26 18:56:36 -0600

    added interface

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,18 +1,15 @@
 public class MagicSquare {
 
-    private int n;
     private int[][] magicGrid;
-    private int magicNumber;
+
 
     public MagicSquare(int n) {
-        magicNumber = n * (n*n + 1) / 2;
+        int magicNumber = n * (n*n + 1) / 2;  
+        
         magicGrid = new int[n][n];
-        for (int r = 0;r < n;r++) {
+        for (int r = 0;r < n;r++) {           //loop goes to each individual location in the grid 
             for (int c=0;c<n; c++) {
-                while (magicGrid==null) {
-                    
-                }
             }
-        }
+        }     
     }
 }
