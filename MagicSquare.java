3dd9b2b5df36299493779f0ec1679b667a6f9a37
gavinbarragancd /Users/gavinbarragan/cs121-2024-sp-activity1-gavinbarragan/cs121-2024-sp-activity2-gavinbarragan/a2.1-TemperatureConversion/ && javac TemperatureConversion.java && java TemperatureConversion <gavinbarragan@u.boisestate.edu>
2024-08-28 11:45:45 -0600

    added the methods

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,15 +1,73 @@
-public class MagicSquare {
 
-    private int[][] magicGrid;
+
+
+
+public class MagicSquare implements MagicSquareInterface {
+
+    private int[][] Matrix;
 
 
     public MagicSquare(int n) {
-        int magicNumber = n * (n*n + 1) / 2;  
         
-        magicGrid = new int[n][n];
+        
+        Matrix = new int[n][n];
         for (int r = 0;r < n;r++) {           //loop goes to each individual location in the grid 
             for (int c=0;c<n; c++) {
             }
-        }     
+        }
     }
+
+
+    @Override
+    public boolean isMagicSquare() {
+        int magicNumber = Matrix.length * (Matrix.length*Matrix.length + 1) / 2;
+        int magicSum = 0;
+
+        for (int r = 0;r < Matrix.length;r++) {           //loop goes to each individual location in the grid 
+            for (int c=0;c<Matrix.length; c++) {
+                magicSum += Matrix[r][c];
+            }
+        }
+
+        if (magicNumber == magicSum) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public int[][] getMatrix() {
+        int[][] copyMatrix = new int[Matrix.length][Matrix.length]; // creates a copy of the matrix with the right dimensions
+        
+        for (int r = 0;r < Matrix.length;r++) {           //loop goes to each individual location in the grid 
+            for (int c=0;c<Matrix.length; c++) {
+                copyMatrix[r][c] = Matrix[r][c];
+            }
+        }
+        
+        return copyMatrix;
+
+    }
+
+    @Override
+	public String toString() {
+        String magicString = " The matrix\n";
+        for (int r = 0;r < Matrix.length;r++) {  
+            magicString += "   ";
+            for (int c=0;c<Matrix.length; c++) {
+                magicString += Matrix[r][c];
+            }
+            magicString += "\n";
+        }
+        if () {
+
+        }
+        else{
+            
+        }
+
+
 }
--- MagicSquareDriver.java
+++ MagicSquareDriver.java
@@ -1,3 +1,5 @@
 public class MagicSquareDriver {
-    
+    public static void main(String[] args) {
+
+    }
 }
